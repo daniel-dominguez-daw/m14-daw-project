e1509161414b528baf782eba9c72b2a055e5b4ff
zebnat <zebnat@gmail.com>
2020-11-27 01:58:24 +0100

    feat: added login authorization step and login token retrival step

--- src/App.js
+++ src/App.js
@@ -17,16 +17,106 @@ import {
     Switch,
     Route,
     Link as RLink,
-    useLocation
+    useLocation,
+    useParams
 } from 'react-router-dom';
+
+import {useState, useEffect } from 'react';
+import axios from 'axios';
 
 import AWS from 'aws-sdk';
 
 
-const DOMAINLOGIN = 'danieldm14.auth.us-east-1.amazoncognito.com';
-const LOGINCLIENT = '7tpgrtnd1r2nsej9nt0gg8cj1a';
+const DOMAINLOGIN = 'https://danieldm14.auth.us-east-1.amazoncognito.com/';
+const APPCLIENT = '7tpgrtnd1r2nsej9nt0gg8cj1a';
 const LOGINREDIRECT = 'https://jdam14dual.org:3000/welcome/';
-const LOGINURL = `https://${DOMAINLOGIN}/login?response_type=token&client_id=${LOGINCLIENT}&redirect_uri=${LOGINREDIRECT}`;
+const APPSECRET = '167m23isdch0ovhf0hfo64k491rah95mqtrng0aqud9f19e3gc4t';
+//const LOGINURL = `https://${DOMAINLOGIN}/login?response_type=code&client_id=${APPCLIENT}&redirect_uri=${LOGINREDIRECT}`;
+
+class Oauth2AWSAPI {
+    constructor(axiosInstance, baseApi, client){
+        this.axios = axiosInstance;
+        this.baseApi = baseApi;
+        this.client = client;
+    }
+
+    authorizationEndPoint(redirectUri){
+        const params = {
+            response_type: 'code',
+            client_id: this.client,
+            redirect_uri: redirectUri,
+            state: 'asdf',
+            identity_provider: 'Google'
+        };
+
+        const uri = this.baseApi + 'oauth2/authorize';
+        return [uri, params];
+    }
+
+    authTokenEndPoint(codeGrant){
+        /*
+        Content-Type
+        Must always be 'application/x-www-form-urlencoded'.
+        */
+        const headers = {
+            'Authorization': 'Basic '+ btoa(APPCLIENT + ":" + APPSECRET),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+        
+
+        const params = {
+            'grant_type': 'authorization_code',
+            'code': codeGrant,
+            'redirect_uri' : LOGINREDIRECT,
+            'client_id': APPCLIENT
+        };
+        const uri = this.baseApi + 'oauth2/token';
+        return [uri, params, headers];
+    }
+
+    performRequest(data, method) {
+        console.log("BLARG");
+        console.log(data[1]);
+
+        let obj = {
+            method: method,
+            url: data[0],
+            data: data[1],
+            transformRequest: (data, headers) => {
+                let str = [];
+                for( let x in data) {
+                    str.push(x + '=' + encodeURIComponent(data[x]));
+                }
+                return str.join("&");
+            }
+
+        };
+
+        if(data[2] !== undefined) {
+            obj.headers = data[2];
+        }
+
+        console.log(obj.headers);
+
+        this.axios(
+            obj
+        );
+    }
+
+    toGetUri(arr){
+        const params = [];
+        let pair;
+        for(let x in arr[1]) {
+            pair = x + '=' + arr[1][x];
+            params.push(pair);
+        }
+
+        return arr[0] + '?' + params.join("&");
+    }
+}
+
+const api = new Oauth2AWSAPI(axios, DOMAINLOGIN, APPCLIENT);
+const LOGINURL = api.toGetUri(api.authorizationEndPoint(LOGINREDIRECT));
 
 // Initialize the Amazon Cognito credentials provider
 
@@ -48,15 +138,28 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Welcome(props) {
-    const { hash } = useLocation();
-    const hashParams = hash.replace("#", "").split("&").map(e => e.split("="));
+    const { search } = useLocation();
+    const getParams = search.replace("?", "").
+                        split("&").
+                            map(e=> e.split("="));
+    //const hashParams = hash.replace("#", "").split("&").map(e => e.split("="));
+    // On component mount check if we have code GET param in url and set is as an authCode state
+    useEffect(() => {
+        const urlParams = new URLSearchParams(search);
+        if(urlParams.has('code')) {
+            console.log('CODE: ' + urlParams.get('code'));
+
+            api.performRequest(
+                api.authTokenEndPoint(
+                    urlParams.get('code')), 'post');
+        }
+    }, []);
 
     return (
         <>
-            <p>Welcome User!</p>
             <p>Your credentials:</p>
             <table>
-                {hashParams.map(e=> <tr><td>{e[0]}</td><td>{e[1]}</td></tr>)}
+                {getParams.map(e=> <tr><td>{e[0]}</td><td>{e[1]}</td></tr>)}
             </table>
         </>
     );
