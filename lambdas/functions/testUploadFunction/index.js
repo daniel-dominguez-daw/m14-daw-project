d30d1fc1d8c1281c9b68fbe11e82e4561cd6318c
zebnat <zebnat@gmail.com>
2021-03-08 15:20:08 +0100

    test aws sdk from lambda

--- lambdas/functions/testUploadFunction/index.js
+++ lambdas/functions/testUploadFunction/index.js
@@ -1,10 +1,11 @@
 const isOdd = require('is-odd');
+const AWS = require('aws-sdk');
 
 exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('is odd 3? '+ isOdd(3)),
+        body: JSON.stringify('is odd 3? '+ isOdd(3) + 'sdk:' + AWS.VERSION),
     };
     return response;
 };
